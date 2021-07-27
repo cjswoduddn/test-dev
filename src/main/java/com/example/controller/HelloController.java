6f31128526eeb58349c325523e1bdd471107962c
poirier.choi <poirier.choi@poirierchoiui-MacBookPro.local>
2021-07-27 11:15:29 +0900

    ol 3

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -9,6 +9,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         System.out.println("added!");
+        System.out.println("conn pull req");
         return "Heiio, anotoher-World!";
     }
 }
