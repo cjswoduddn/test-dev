62dfa4f45a0694ca8c2c6878927d5e185726e9b1
cjswoudddn <ma8511@naver.com>
2021-08-29 20:20:57 +0900

    first branch commit

--- src/main/java/com/example/testenv/second/SecondController.java
+++ src/main/java/com/example/testenv/second/SecondController.java
@@ -1,5 +1,7 @@
 package com.example.testenv.second;
 
+import com.example.testenv.first.FirstController;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("second")
 @RestController
+@RequiredArgsConstructor
 public class SecondController {
+
+    private final FirstController firstController;
 
     @GetMapping
     public String second(){
